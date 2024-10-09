163a4e1c86914e233b0fd1f26caaa0a186f5f865
IamStarlight <108684310+IamStarlight@users.noreply.github.com>
2024-10-09 11:44:22 +0800

    feat: Add Wawa Office model and scrolling ctrl

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,20 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, ScrollControls } from "@react-three/drei"
 import { BoxGeometry, MeshNormalMaterial } from "three"
+import { Office } from "./Office";
+import { Face } from "three/examples/jsm/Addons.js";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      //添加环境光 @intensity: 强度
+      <ambientLight intensity={1} />
+      //添加相机控制器 @enableZoom: 是否允许缩放
+      <OrbitControls enableZoom={false} />
+      //添加滚动控制器 @pages: 页面数量 @damping: 阻尼
+      <ScrollControls pages={3} damping={0.25} >
+        <Office />
+      </ScrollControls>
+        
     </>
   );
 }
